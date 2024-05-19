95681f6ead6149f044b7c3702449d9e2be8510a5
Jack Hermanson <jack.hermanson@live.com>
2024-05-18 23:58:08 -0600

    blank admin dashboard

--- main/modules/admin/routes.py
+++ main/modules/admin/routes.py
@@ -1,9 +1,8 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template
 
 admin = Blueprint("admin", __name__, url_prefix="/admin")
 
 
 @admin.route("/")
 def index():
-    return "admin index"
+    return render_template("admin/index.html")
