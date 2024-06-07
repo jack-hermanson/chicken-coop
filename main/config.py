7f34d18a447964cc2de92063235817445ce574d3
Jack Hermanson <jack.hermanson@live.com>
2024-06-06 20:30:03 -0600

    logger updates

--- main/config.py
+++ main/config.py
@@ -9,8 +9,8 @@ class Config:
     TEMPLATES_AUTO_RELOAD = True
     environment = os.getenv("ENVIRONMENT")
     SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     if os.environ.get("DATABASE_URL"):
         SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
     else:
         SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
