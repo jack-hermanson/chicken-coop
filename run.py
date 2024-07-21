4eeac6f626fc4b87f6c45b9f6fee695ec3ea3189
Jack Hermanson <jack.hermanson@live.com>
2024-07-21 13:03:22 -0600

    fix port

--- run.py
+++ run.py
@@ -1,24 +1,8 @@
-import argparse
+import os
 from main import create_app
 
-
-def main():
-    # Create the Flask app
-    app = create_app()
-
-    # Set up argument parsing
-    parser = argparse.ArgumentParser(description="Run the Flask app.")
-    parser.add_argument(
-        "--port",
-        type=int,
-        default=5025,
-        help="Port number on which the app should run"
-    )
-    args = parser.parse_args()
-
-    # Run the app with the specified port
-    app.run(debug=True, port=args.port)
-
+app = create_app()
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5025))
+    app.run(debug=True, port=port)
